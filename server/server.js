c356100ef5c2e7d75c5aa387743a3dd7ae900bfc
shahzaib <shaibii4292@gmail.com>
2026-06-08 15:28:53 +0500

    new code

--- server/server.js
+++ server/server.js
@@ -27,3 +27,63 @@ app.post('/login', (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+
+
+app.get('/balance/:userId', (req, res) => {
+  const userId = req.params.userId;
+
+  const sql = `
+    SELECT balance 
+    FROM accounts
+    WHERE user_id = ?
+  `;
+
+  db.query(sql, [userId], (err, result) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    if (result.length > 0) {
+      res.json({
+        balance: result[0].balance
+      });
+    } else {
+      res.status(404).json({
+        message: 'Account not found'
+      });
+    }
+  });
+});
+
+
+
+//transfer money
+
+app.post('/transfer', (req, res) => {
+  const { fromId, toUsername, amount } = req.body;
+
+  if (amount <= 0) {
+    return res.status(400).json({ message: 'Invalid amount' });
+  }
+
+  // 1. Get receiver
+  const findUser = "SELECT * FROM users WHERE username = ?";
+  db.query(findUser, [toUsername], (err, result) => {
+    if (err) return res.status(500).json(err);
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: 'Receiver not found' });
+    }
+
+    const toUser = result[0];
+
+    
+    const deduct = "UPDATE accounts SET balance = balance - ? WHERE user_id = ?";
+    db.query(deduct, [amount, fromId]);
+
+    const add = "UPDATE accounts SET balance = balance + ? WHERE user_id = ?";
+    db.query(add, [amount, toUser.id]);
+
+    res.json({ message: 'Transfer successful' });
+  });
+});
